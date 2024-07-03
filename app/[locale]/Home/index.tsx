d61443f4183751a5a161ee53136157ba840767ed
Awais <Awaisaziz305@gmail.com>
2024-07-03 12:06:56 +0500

    aaa

--- app/[locale]/Home/index.tsx
+++ app/[locale]/Home/index.tsx
@@ -1,12 +1,9 @@
-import type {locales } from "@/i18n";
-import { getTranslations } from "next-intl/server";
 import HomeHero from "../Home/home-hero";
 import HomeAddress from "../Home/address";
 
 
 
 export default async function HomePage() {
-  const t = await getTranslations();
   return (
     <>
       <HomeHero  />
