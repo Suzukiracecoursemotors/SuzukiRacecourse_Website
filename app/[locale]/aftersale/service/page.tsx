893abcd26f87cdfb8008c4a92f8d3e2b1b280243
Awais <Awaisaziz305@gmail.com>
2024-09-05 19:39:34 +0500

    aaa

--- app/[locale]/aftersale/service/page.tsx
+++ app/[locale]/aftersale/service/page.tsx
@@ -1,275 +1,131 @@
-import { Locale } from "@/i18n";
-import { getTranslations } from "next-intl/server";
 import Image from "next/image";
 import React from "react";
-import CUCbanner from "../../public/WhatsApp Image 2024-07-08 at 11.26.08 AM.jpeg";
-import Carosil from "../../common/carosil";
-
-type PricingPageProps = {
-  params: { locale: Locale };
+import homepic from "./WhatsApp Image 2024-09-05 at 11.10.02 AM.jpeg";
+const config = {
+  title: "Mechanical Workshop",
+  note: "NOTE: √: Applicable X: Not Applicable",
 };
-
-export default async function CUC({ params: { locale } }: PricingPageProps) {
-  const t = await getTranslations({ locale, namespace: "" });
-
+export default async function MechanicalWorkshop() {
   return (
     <article>
-      <section className="py-10 py-xl-15">
+      <section className="py-10">
+        <Image
+          src={homepic}
+          layout="responsive"
+          alt=" background-image background "
+        />{" "}
         <div className="container">
           <div className="row justify-content-center">
             <div className="col-lg-10 col-xl-8">
               <div>
-                <h1 className="display-1 text-blue">Mechanical WorkShop </h1>
+                <h1 className="display-1 text-blue">{config.title} </h1>
                 <div>
                   <p className="fs-lg text-secondary">
-                    Welcome to Suzuki Racecourse Motor Sales Team, your trusted
-                    source for high-quality Suzuki vehicles and exceptional
-                    customer service. Our dealership offers a wide range of new
-                    and pre-owned Suzuki cars, SUVs, and trucks, ensuring you
-                    find the perfect vehicle to meet your needs. Our
-                    knowledgeable sales team is dedicated to providing a
-                    seamless car buying experience, with transparent pricing and
-                    flexible financing options. Whether youre looking for the
-                    latest models or certified pre-owned vehicles, we have
-                    something for everyone.
+                    At Suzuki, our mechanical workshops are designed to provide
+                    top-tier service and expertise for all Suzuki vehicles.
+                    Whether its routine maintenance, engine diagnostics, or
+                    complex repairs, our skilled technicians ensure that your
+                    vehicle is treated with the utmost care. We believe in
+                    delivering excellence through advanced tools, genuine Suzuki
+                    parts, and a team of certified professionals.
                   </p>
                 </div>
               </div>
               <div>
                 <div>
-                  <h5>Insurance</h5>
+                  <h5>Customer Choice of Engine Oil</h5>
                   <p className="fs-lg text-secondary">
-                    Welcome to Suzuki Racecourse Motor Sales Team, your trusted
-                    source for high-quality Suzuki vehicles and exceptional
-                    customer service. Our dealership offers a wide range of new
-                    and pre-owned Suzuki cars, SUVs, and trucks, ensuring you
-                    find the perfect vehicle to meet your needs. Our
-                    knowledgeable sales team is dedicated to providing a
-                    seamless car buying experience, with transparent pricing and
-                    flexible financing options. Whether youre looking for the
-                    latest models or certified pre-owned vehicles, we have
-                    something for everyone.
+                    We prioritize our customers preferences when it comes to
+                    engine oil selection. Suzuki vehicles require precision and
+                    high-quality products to maintain their performance. Our
+                    workshop offers a variety of engine oils, tailored to meet
+                    the diverse needs of each Suzuki model.
                   </p>
                 </div>
               </div>
 
               <div>
                 <div>
+                  <h5>Comprehensive Services</h5>
                   <p className="fs-lg text-secondary">
-                    Welcome to Suzuki Racecourse Motor Sales Team, your trusted
-                    source for high-quality Suzuki vehicles and exceptional
-                    customer service. Our dealership offers a wide range of new
-                    and pre-owned Suzuki cars, SUVs, and trucks, ensuring you
-                    find the perfect vehicle to meet your needs. Our
-                    knowledgeable sales team is dedicated to providing a
-                    seamless car buying experience, with transparent pricing and
-                    flexible financing options. Whether youre looking for the
-                    latest models or certified pre-owned vehicles, we have
-                    something for everyone.
+                    We offer a wide range of services at our mechanical
+                    workshop, including:
                   </p>
                 </div>
+                <div>
+                  <ul className="list-group">
+                    <li className="list-group-item d-flex align-items-center">
+                      <div className="icon-box icon-box-sm bg-opaque-green rounded-circle me-2">
+                        <i className="bi bi-check2 text-green"></i>
+                      </div>
+                      Engine Overhauling{" "}
+                    </li>
+                    <li className="list-group-item d-flex align-items-center">
+                      <div className="icon-box icon-box-sm bg-opaque-green rounded-circle me-2">
+                        <i className="bi bi-check2 text-green"></i>
+                      </div>
+                      Transmission Repair{" "}
+                    </li>
+                    <li className="list-group-item d-flex align-items-center">
+                      <div className="icon-box icon-box-sm bg-opaque-green rounded-circle me-2">
+                        <i className="bi bi-check2 text-green"></i>
+                      </div>
+                      Brake System Maintenance{" "}
+                    </li>
+                    <li className="list-group-item d-flex align-items-center">
+                      <div className="icon-box icon-box-sm bg-opaque-green rounded-circle me-2">
+                        <i className="bi bi-check2 text-green"></i>
+                      </div>
+                      Suspension Tuning{" "}
+                    </li>
+                    <li className="list-group-item d-flex align-items-center">
+                      <div className="icon-box icon-box-sm bg-opaque-green rounded-circle me-2">
+                        <i className="bi bi-check2 text-green"></i>
+                      </div>
+                      Electrical Diagnostics{" "}
+                    </li>
+                    <li className="list-group-item d-flex align-items-center">
+                      <div className="icon-box icon-box-sm bg-opaque-green rounded-circle me-2">
+                        <i className="bi bi-check2 text-green"></i>
+                      </div>
+                      Air Conditioning Repair
+                    </li>
+                  </ul>
+                </div>
               </div>
-
+              <h1 className="display-1 text-blue">{config.title} </h1>
               <div>
                 <div>
-                  <h5>Insurance Method</h5>
+                  <h5>Genuine Parts</h5>
                   <p className="fs-lg text-secondary">
-                    Welcome to Suzuki Racecourse Motor Sales Team, your trusted
-                    source for high-quality Suzuki vehicles and exceptional
-                    customer service. Our dealership offers a wide range of new
-                    and pre-owned Suzuki cars, SUVs, and trucks, ensuring you
-                    find the perfect vehicle to meet your needs. Our
-                    knowledgeable sales team is dedicated to providing a
-                    seamless car buying experience, with transparent pricing and
-                    flexible financing options. Whether youre looking for the
-                    latest models or certified pre-owned vehicles, we have
-                    something for everyone.
+                    We use only authentic Suzuki parts to maintain the integrity
+                    of your vehicle.
                   </p>
                 </div>
               </div>
               <div>
                 <div>
+                  <h5>Certified Technicians</h5>
                   <p className="fs-lg text-secondary">
-                    Welcome to Suzuki Racecourse Motor Sales Team, your trusted
-                    source for high-quality Suzuki vehicles and exceptional
-                    customer service. Our dealership offers a wide range of new
-                    and pre-owned Suzuki cars, SUVs, and trucks, ensuring you
-                    find the perfect vehicle to meet your needs. Our
-                    knowledgeable sales team is dedicated to providing a
-                    seamless car buying experience, with transparent pricing and
-                    flexible financing options. Whether youre looking for the
-                    latest models or certified pre-owned vehicles, we have
-                    something for everyone.
+                    Our team is trained to handle every Suzuki model with
+                    expertise.{" "}
                   </p>
                 </div>
               </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="py-15 py-xl-20 bg-black inverted">
-        <div className="container">
-          <div className="row align-items-end mb-5">
-            <div className="col-lg-8 mb-2 mb-lg-0">
-              <h2>Frequently asked questions</h2>
-            </div>
-          </div>
-          <div className="row mb-8">
-            <div className="col-12">
-              <div
-                className="accordion accordion-highlight border-top border-bottom"
-                id="accordion-1"
-              >
-                <div className="accordion-item">
-                  <h2 className="accordion-header" id="heading-1-1">
-                    <button
-                      className="accordion-button collapsed"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target="#collapse-1-1"
-                      aria-expanded="false"
-                      aria-controls="collapse-1-1"
-                    >
-                      What is the difference between an editor and a viewer ?
-                    </button>
-                  </h2>
-                  <div
-                    id="collapse-1-1"
-                    className="accordion-collapse collapse"
-                    aria-labelledby="heading-1-1"
-                    data-bs-parent="#accordion-1"
-                  >
-                    <div className="accordion-body">
-                      <p className="text-secondary">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Dolore cumque quos libero, ratione non hic nisi autem
-                        dicta saepe asperiores repellendus quis ab est.
-                        Repellendus dicta itaque aliquid officiis animi?
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="accordion-item">
-                  <h2 className="accordion-header" id="heading-1-2">
-                    <button
-                      className="accordion-button collapsed"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target="#collapse-1-2"
-                      aria-expanded="false"
-                      aria-controls="collapse-1-2"
-                    >
-                      What if more editors are added to my team each month ?
-                    </button>
-                  </h2>
-                  <div
-                    id="collapse-1-2"
-                    className="accordion-collapse collapse"
-                    aria-labelledby="heading-1-2"
-                    data-bs-parent="#accordion-1"
-                  >
-                    <div className="accordion-body">
-                      <p className="text-secondary">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Voluptates quisquam aut quod culpa fugit voluptatibus
-                        sunt, obcaecati eveniet impedit eligendi voluptatem
-                        reiciendis cum consequatur sit quia mollitia. Nam,
-                        tempora tempore!
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="accordion-item">
-                  <h2 className="accordion-header" id="heading-1-3">
-                    <button
-                      className="accordion-button collapsed"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target="#collapse-1-3"
-                      aria-expanded="false"
-                      aria-controls="collapse-1-3"
-                    >
-                      What payment methods do you offer ?
-                    </button>
-                  </h2>
-                  <div
-                    id="collapse-1-3"
-                    className="accordion-collapse collapse"
-                    aria-labelledby="heading-1-3"
-                    data-bs-parent="#accordion-1"
-                  >
-                    <div className="accordion-body">
-                      <p className="text-secondary">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Est ullam soluta ipsam, commodi atque tempore debitis
-                        quaerat molestias neque aperiam, doloribus vero?
-                        Suscipit et dignissimos minus, vel distinctio odit.
-                        Earum.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="accordion-item">
-                  <h2 className="accordion-header" id="heading-1-4">
-                    <button
-                      className="accordion-button collapsed"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target="#collapse-1-4"
-                      aria-expanded="false"
-                      aria-controls="collapse-1-4"
-                    >
-                      What is your refund policy?
-                    </button>
-                  </h2>
-                  <div
-                    id="collapse-1-4"
-                    className="accordion-collapse collapse"
-                    aria-labelledby="heading-1-4"
-                    data-bs-parent="#accordion-1"
-                  >
-                    <div className="accordion-body">
-                      <p className="text-secondary">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Est ullam soluta ipsam, commodi atque tempore debitis
-                        quaerat molestias neque aperiam, doloribus vero?
-                        Suscipit et dignissimos minus, vel distinctio odit.
-                        Earum.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="accordion-item">
-                  <h2 className="accordion-header" id="heading-1-5">
-                    <button
-                      className="accordion-button collapsed"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target="#collapse-1-5"
-                      aria-expanded="false"
-                      aria-controls="collapse-1-5"
-                    >
-                      Do you have educational or non-profit discounts ?
-                    </button>
-                  </h2>
-                  <div
-                    id="collapse-1-5"
-                    className="accordion-collapse collapse"
-                    aria-labelledby="heading-1-5"
-                    data-bs-parent="#accordion-1"
-                  >
-                    <div className="accordion-body">
-                      <p className="text-secondary">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Est ullam soluta ipsam, commodi atque tempore debitis
-                        quaerat molestias neque aperiam, doloribus vero?
-                        Suscipit et dignissimos minus, vel distinctio odit.
-                        Earum.
-                      </p>
-                    </div>
-                  </div>
+              <div>
+                <div>
+                  <h5>State-of-the-art Equipment</h5>
+                  <p className="fs-lg text-secondary">
+                    Our workshops are equipped with the latest tools and
+                    technology to ensure accurate and efficient repairs.
+                  </p>
                 </div>
               </div>
+              <p>
+                By choosing Suzuki, youre ensuring that your vehicle receives
+                the best care possible, keeping it in peak condition for years
+                to come
+              </p>
             </div>
           </div>
         </div>
