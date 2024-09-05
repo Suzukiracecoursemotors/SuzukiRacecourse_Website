9968bf1564a054bb88981788b46e1e415dba7143
Awais <Awaisaziz305@gmail.com>
2024-09-05 13:00:56 +0500

    Awais

--- app/[locale]/aboutus/page.tsx
+++ app/[locale]/aboutus/page.tsx
@@ -19,7 +19,6 @@ export default async function AboutUspage({
           <div className="row align-items-center justify-content-center justify-content-lg-between">
             <div
               className="col-md-8 col-lg-5 mb-5 mb-xl-0"
-              data-aos="fade-left"
               data-aos-delay="100"
             >
               <Image
@@ -45,9 +44,9 @@ export default async function AboutUspage({
                     At Suzuki Racecourse Motors, our customers are at the heart
                     of everything we do. We are committed to delivering vehicles
                     and services that meet your highest expectations, with a
-                    focus on innovation, quality, and reliability. Whether
-                    you're purchasing your first car or returning for another,
-                    we are here to ensure your experience is seamless and
+                    focus on innovation, quality, and reliability. Whether you
+                    are purchasing your first car or returning for another, we
+                    are here to ensure your experience is seamless and
                     satisfying. Your trust drives us to constantly improve, and
                     we look forward to continuing this journey with you. Thank
                     you for choosing Suzuki Racecourse Motors.
