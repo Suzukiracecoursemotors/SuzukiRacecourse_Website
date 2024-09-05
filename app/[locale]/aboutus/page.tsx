8f7e5da9af63949071be8ea2345e982db63a19fd
Awais <Awaisaziz305@gmail.com>
2024-09-05 12:55:55 +0500

    Awais

--- app/[locale]/aboutus/page.tsx
+++ app/[locale]/aboutus/page.tsx
@@ -1,6 +1,9 @@
 import { Locale } from "@/i18n";
 import Gallery from "../common/gallery";
 import { getTranslations } from "next-intl/server";
+import Image from "next/image";
+import Moazzmpic from "./WhatsApp Image 2024-09-05 at 11.10.02 AM.jpeg";
+import AimsandMissions from "../Home/about_aim";
 type GalleryProps = {
   params: { locale: Locale };
 };
@@ -11,6 +14,51 @@ export default async function AboutUspage({
   const t = await getTranslations({ locale, namespace: "page" });
   return (
     <>
+      <section className="py-15 py-xl-20 bg-light">
+        <div className="container">
+          <div className="row align-items-center justify-content-center justify-content-lg-between">
+            <div
+              className="col-md-8 col-lg-5 mb-5 mb-xl-0"
+              data-aos="fade-left"
+              data-aos-delay="100"
+            >
+              <Image
+                src={Moazzmpic}
+                alt="awais_image"
+                width={600}
+                height={500}
+                layout="responsive"
+              />
+            </div>
+            <div
+              className="col-lg-6 text-center text-lg-start"
+              data-aos="fade-right"
+              data-aos-delay="100"
+            >
+              <h2>Syed Moazzam Hussain Shah</h2>
+              <ul className="list-group list-group-separated">
+                <li className="list-group-item py-4">
+                  <p className="text-muted lead">
+                    <span className="text-black">
+                      Message from the Director
+                    </span>{" "}
+                    At Suzuki Racecourse Motors, our customers are at the heart
+                    of everything we do. We are committed to delivering vehicles
+                    and services that meet your highest expectations, with a
+                    focus on innovation, quality, and reliability. Whether
+                    you're purchasing your first car or returning for another,
+                    we are here to ensure your experience is seamless and
+                    satisfying. Your trust drives us to constantly improve, and
+                    we look forward to continuing this journey with you. Thank
+                    you for choosing Suzuki Racecourse Motors.
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+      <AimsandMissions />
       <Gallery isGrey={true} title={t("gallery.title")} />
     </>
   );
--- app/[locale]/common/gallery.tsx
+++ app/[locale]/common/gallery.tsx
@@ -1,32 +1,36 @@
 "use client";
 import Image from "next/image";
-import { useMemo, useState } from "react";
-// import {
-//   allImgs,
-//   cosmeticsImgs,
-//   interriorImgs,
-//   massagesImgs,
-// } from "@/lib/utils";
+import { useState } from "react";
 import { useTranslations } from "next-intl";
 import Galaryimg from "../public/PurchaseOfferuptoRs.50,000forWagonR_1_011802.png";
+
 interface GalleryProps {
   isGrey: boolean;
   title: string;
 }
-type GalleryType = "Gallery";
-const types: GalleryType[] = ["Gallery"];
+
+const images = [
+  {
+    src: Galaryimg,
+    alt: "Image 1 description",
+  },
+  {
+    src: Galaryimg,
+    alt: "Image 1 description",
+  },
+];
+const galleryTypes = ["Gallery"] as const;
 
 function Gallery({ isGrey, title }: GalleryProps) {
   const t = useTranslations("page");
+  const [selectedType, setSelectedType] = useState(galleryTypes[0]);
 
-  const [selected, setSelected] = useState("all");
-
-  const handleChange = (type: string) => {
-    setSelected(type);
+  const handleTypeChange = (type: any) => {
+    setSelectedType(type);
   };
 
   return (
-    <section className={"py-15 py-xl-20" + (isGrey ? " bg-light" : "")}>
+    <section className={`py-1 py-xl-1${isGrey ? " bg-light" : ""}`}>
       <div className="container mt-5 mt-xl-10">
         <div className="row mb-10 g-3 g-xl-5 align-items-end">
           <div className="col-lg-8">
@@ -34,157 +38,35 @@ function Gallery({ isGrey, title }: GalleryProps) {
           </div>
         </div>
 
-        <div className="mb-5 ">
-          {types.map((type: GalleryType) => {
-            return (
-              <button
-                key={type}
-                className={`btn btn-filter rounded-pill my-1 me-1 ${
-                  selected === type ? "current" : ""
-                }`}
-                onClick={() => handleChange(type)}
-              >
-                {t(`gallery.${type}`)}
-              </button>
-            );
-          })}
+        <div className="mb-5">
+          {galleryTypes.map((type) => (
+            <button
+              key={type}
+              className={`btn btn-filter rounded-pill my-1 me-1 ${
+                selectedType === type ? "current" : ""
+              }`}
+              onClick={() => handleTypeChange(type)}
+            >
+              Gallery{" "}
+            </button>
+          ))}
         </div>
 
-        <div className={`row mb-10 g-3 g-xl-5`} data-isotope="">
-          {/* {selectetImages.map((image, index) => ( */}
-          <div key={""} className={`col-md-6 col-lg-4`}>
-            <div className="media media-image gallery-img-container overlay">
-              <Image
-                src={Galaryimg}
-                width={600}
-                height={600}
-                layout="responsive"
-                style={{ objectFit: "cover" }}
-                alt={""}
-              />
+        <div className="row mb-10 g-3 g-xl-5">
+          {images.map((image, index) => (
+            <div key={index} className="col-md-6 col-lg-4">
+              <div className="media media-image gallery-img-container overlay">
+                <Image
+                  src={image.src}
+                  width={600}
+                  height={600}
+                  layout="responsive"
+                  style={{ objectFit: "cover" }}
+                  alt={image.alt}
+                />
+              </div>
             </div>
-          </div>
-          <div key={""} className={`col-md-6 col-lg-4`}>
-            <div className="media media-image gallery-img-container overlay">
-              <Image
-                src={Galaryimg}
-                width={600}
-                height={600}
-                layout="responsive"
-                style={{ objectFit: "cover" }}
-                alt={""}
-              />
-            </div>
-          </div>
-          <div key={""} className={`col-md-6 col-lg-4`}>
-            <div className="media media-image gallery-img-container overlay">
-              <Image
-                src={Galaryimg}
-                width={600}
-                height={600}
-                layout="responsive"
-                style={{ objectFit: "cover" }}
-                alt={""}
-              />
-            </div>
-          </div>
-          <div key={""} className={`col-md-6 col-lg-4`}>
-            <div className="media media-image gallery-img-container overlay">
-              <Image
-                src={Galaryimg}
-                width={600}
-                height={600}
-                layout="responsive"
-                style={{ objectFit: "cover" }}
-                alt={""}
-              />
-            </div>
-          </div>
-          <div key={""} className={`col-md-6 col-lg-4`}>
-            <div className="media media-image gallery-img-container overlay">
-              <Image
-                src={Galaryimg}
-                width={600}
-                height={600}
-                layout="responsive"
-                style={{ objectFit: "cover" }}
-                alt={""}
-              />
-            </div>
-          </div>
-          <div key={""} className={`col-md-6 col-lg-4`}>
-            <div className="media media-image gallery-img-container overlay">
-              <Image
-                src={Galaryimg}
-                width={600}
-                height={600}
-                layout="responsive"
-                style={{ objectFit: "cover" }}
-                alt={""}
-              />
-            </div>
-          </div>
-          <div key={""} className={`col-md-6 col-lg-4`}>
-            <div className="media media-image gallery-img-container overlay">
-              <Image
-                src={Galaryimg}
-                width={600}
-                height={600}
-                layout="responsive"
-                style={{ objectFit: "cover" }}
-                alt={""}
-              />
-            </div>
-          </div>
-          <div key={""} className={`col-md-6 col-lg-4`}>
-            <div className="media media-image gallery-img-container overlay">
-              <Image
-                src={Galaryimg}
-                width={600}
-                height={600}
-                layout="responsive"
-                style={{ objectFit: "cover" }}
-                alt={""}
-              />
-            </div>
-          </div>
-          <div key={""} className={`col-md-6 col-lg-4`}>
-            <div className="media media-image gallery-img-container overlay">
-              <Image
-                src={Galaryimg}
-                width={600}
-                height={600}
-                layout="responsive"
-                style={{ objectFit: "cover" }}
-                alt={""}
-              />
-            </div>
-          </div>
-          <div key={""} className={`col-md-6 col-lg-4`}>
-            <div className="media media-image gallery-img-container overlay">
-              <Image
-                src={Galaryimg}
-                width={600}
-                height={600}
-                layout="responsive"
-                style={{ objectFit: "cover" }}
-                alt={""}
-              />
-            </div>
-          </div>
-          <div key={""} className={`col-md-6 col-lg-4`}>
-            <div className="media media-image gallery-img-container overlay">
-              <Image
-                src={Galaryimg}
-                width={600}
-                height={600}
-                layout="responsive"
-                style={{ objectFit: "cover" }}
-                alt={""}
-              />
-            </div>
-          </div>
-          {/* ))} */}
+          ))}
         </div>
       </div>
     </section>
