4ab40bdedb1b82253ff3ac86eeec22e0b6d2cb9b
Awais <Awaisaziz305@gmail.com>
2024-09-20 14:37:27 +0500

    sjhjk

--- app/[locale]/infrastructure/page.tsx
+++ app/[locale]/infrastructure/page.tsx
@@ -1,12 +1,14 @@
 import { getTranslations } from "next-intl/server";
 import Departments from "./components/departments";
 import Landingpage from "./components/landing_page";
+import Workflow from "./components/home-workflow";
 
 async function Infrastructure() {
   const t = await getTranslations({ namespace: "page" });
   return (
     <>
-      <Landingpage />
+      <Workflow />
+      {/* <Landingpage /> */}
       <Departments />
     </>
   );
