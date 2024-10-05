4dd10361a33d8abd96a4c8c8b7c99db19e0bd631
Awais <awaisaziz304@gmail.com>
2024-10-05 10:33:21 +0500

    update map

--- data/Constants.ts
+++ data/Constants.ts
@@ -7,7 +7,8 @@ const Constants = {
   Insurance: "+92 341 1990939",
   finance: "+92 341 1990939",
   HR: "+92 341 1990944",
-  GOOGLE_MAP: "info@suzukiracecoursemotors.com.pk",
+  GOOGLE_MAP:
+    "https://www.google.com/maps/dir//299%2FA+Main+Peshawar+Rd,+Rawalpindi,+Punjab+46000/@33.6022897,72.9475839,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x38df94659e659eb5:0x499dee70b9674796!2m2!1d73.0299851!2d33.602317?entry=ttu&g_ep=EgoyMDI0MTAwMi4xIKXMDSoASAFQAw%3D%3D",
 };
 
 export default Constants;
